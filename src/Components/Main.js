88dd6cfbecd8c37485feb563baf24aabdfd7d24d
leehj050211 <leehj050211@gmail.com>
2022-05-30 15:06:41 +0900

    Fix: 메뉴바 반응형 css, 전체적인 css 수정

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -12,18 +12,27 @@ export default function Main(){
     }
     return(
         <div>
-            <nav>
-                <img src={"images/tcpicon.png"} onClick={() => navigate('/')} />
-                <Link to={"/TeamRecruitment"} className={"link"}>팀원 모집</Link>
-                <Link to={"/community"} className={"link"}>커뮤니티</Link>
-                <Link to={"/ourTeam"} className={"link"}>나의 팀</Link>
-                <Link to={"/Canvas"} className={"link"}>캔버스</Link>
-                <div className={"nav--div"}>
-                    <button onClick={Sign_in} className={"continue_BSM"}>BSM 계정으로 계속</button>
-                </div>
-            </nav>
+            <header>
+                <nav className='container'>
+                    <div className="nav--left rows">
+                        <div className='nav--menus rows'>
+                            <img src={"images/tcpicon.png"} className="nav--logo" onClick={() => navigate('/')} />
+                            <button onClick={Sign_in} className="continue_BSM">BSM 계정으로 계속</button>
+                        </div>
+                        <div className='rows'>
+                            <Link to={"/TeamRecruitment"} className="nav--link">팀원 모집</Link>
+                            <Link to={"/community"} className="nav--link">커뮤니티</Link>
+                            <Link to={"/ourTeam"} className="nav--link">나의 팀</Link>
+                            <Link to={"/Canvas"} className="nav--link">캔버스</Link>
+                        </div>
+                    </div>
+                    <div className="nav--right">
+                        <button onClick={Sign_in} className="continue_BSM">BSM 계정으로 계속</button>
+                    </div>
+                </nav>
+            </header>
             <section>
-                <div className={"imgdiv"}>
+                <div className="imgdiv">
                     <img src={"images/teamimg1.png"} className="sec--img"/>
                 </div>
                 <button className="sec--button" onClick={make_it_team}>팀 구하러 가기</button>
