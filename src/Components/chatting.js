b10d8687d50fe57a238ac075bef477e48164a2d0
leehj050211 <leehj050211@gmail.com>
2022-07-11 17:22:12 +0900

    Add: 채팅 리스트 API 연동

--- src/Components/chatting.js
+++ src/Components/chatting.js
@@ -1,8 +1,14 @@
+import React from 'react';
+import axios from 'axios';
 import {TeamHeader, Sidebar, SecSideBar} from '../allFiles'
 import '../styles/chatting.css';
 
-export default function Chatting(props)
-{
+const teamId = '1';
+const roomId = '1';
+
+export default function Chatting(props) {
+    const [chatList, setChatList] = React.useState([]);
+
     const send = () => {
         console.log('실행')
     }
@@ -12,6 +18,27 @@ export default function Chatting(props)
         }
     }
 
+    React.useEffect(() => {
+        InitChatList();
+    }, []);
+    
+    const InitChatList = async () => {
+        try {
+            const chatData = (await getChatList(teamId, roomId)).data.reverse();
+            // 채팅 목록이 없으면
+            if (!chatData.length) {
+                return;
+            }
+            setChatList(chatData);
+        } catch (error) {
+            alert(error.response?.data?.message);
+        }
+    }
+
+    const getChatList = (teamId, roomId, startChatId = 0) => {
+        return axios.get(`/api/chat/${teamId}/${roomId}/${startChatId}`, {withCredentials: true}); 
+    }
+
     return(
         <div className='chatting-div'>
             <TeamHeader user={props.user} />
@@ -19,48 +46,14 @@ export default function Chatting(props)
             <SecSideBar/>
             <div className='chatting-main-div'>
                 <span className='chatting-channel'>채팅</span>
-                <div className='chatting-chat-div'>
-                    <div className='user-chatting'>
-                        <div className='chatting-userIcon'></div>dk
-                        <span className='chatting-speechBubble'>ㅎㅇ</span>
-                    </div>
-                    <div className='user-chatting'>
-                        <div className='chatting-userIcon'></div>
-                        <span className='chatting-speechBubble'>아무말아무말아무말아무말아무말아무말아무말아무말아무말아무말아무말아무말아무말아무말아무말아무말아무말아무말아무말</span>
-                    </div>
-                    <div className='user-chatting'>
-                        <div className='chatting-userIcon'></div>
-                        <span className='chatting-speechBubble'>테스트</span>
-                    </div>
-                    <div className='user-chatting'>
-                        <div className='chatting-userIcon'></div>
-                        <span className='chatting-speechBubble'>테스트</span>
-                    </div>
-                    <div className='user-chatting'>
-                        <div className='chatting-userIcon'></div>
-                        <span className='chatting-speechBubble'>테스트</span>
-                    </div>
-                    <div className='user-chatting'>
-                        <div className='chatting-userIcon'></div>
-                        <span className='chatting-speechBubble'>테스트</span>
-                    </div>
-                    <div className='user-chatting'>
-                        <div className='chatting-userIcon'></div>
-                        <span className='chatting-speechBubble'>테스트</span>
-                    </div>
-                    <div className='user-chatting'>
-                        <div className='chatting-userIcon'></div>
-                        <span className='chatting-speechBubble'>테스트</span>
-                    </div>
-                    <div className='user-chatting'>
-                        <div className='chatting-userIcon'></div>
-                        <span className='chatting-speechBubble'>테스트</span>
-                    </div>
-                    <div className='user-chatting'>
-                        <div className='chatting-userIcon'></div>
-                        <span className='chatting-speechBubble'>테스트</span>
-                    </div>
-                </div>
+                <div className='chatting-chat-div'>{
+                    chatList.map((chat) => (
+                        <div className='user-chatting'>
+                            <div className='chatting-userIcon'></div>
+                            <span className='chatting-speechBubble'>{chat.content}</span>
+                        </div>
+                    ))
+                }</div>
                 <div className='chatting-input'>
                     <input type="text" placeholder='보낼 메세지를 입력하세요.' className='chatting-input--input' onKeyDown={onKeyDown} />
                     <img src="images/chat-submit.png" alt="icon" className='chatting-input--img' onClick={send}/>
