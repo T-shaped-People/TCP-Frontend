1973be1106c6bfecf5ffa63097e7e3986dfd590c
leehj050211 <leehj050211@gmail.com>
2022-09-01 20:56:51 +0900

    ADD: ajax 공통 처리 함수

---
+++ src/util/ajax.ts
@@ -0,0 +1,38 @@
+import axios, { AxiosError, AxiosPromise } from "axios"
+
+enum HttpMethod {
+    GET,
+    POST,
+    PUT,
+    DELETE
+}
+
+interface Ajax {
+    url: string,
+    method: HttpMethod,
+    payload?: any,
+    errorCallback?: (errorData: AxiosError) => void
+}
+
+export const ajax = async <T> ({
+    url,
+    method,
+    payload,
+    errorCallback
+}: Ajax): Promise<T> => {
+    try {
+        const rawRes = ((): AxiosPromise<T> => {
+            switch (method) {
+                case HttpMethod.GET: return axios.get(url);
+                case HttpMethod.POST: return axios.post(url, payload);
+                case HttpMethod.PUT: return axios.put(url, payload);
+                case HttpMethod.DELETE: return axios.delete(url);
+            }
+        })();
+        return (await rawRes).data;
+    } catch (error) {
+        if (error instanceof AxiosError) {
+            errorCallback(error);
+        }
+    }
+}
