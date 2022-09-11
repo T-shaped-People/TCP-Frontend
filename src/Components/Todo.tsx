bb2fb7d7f3c7651b30896f17f9fa93904f9719f1
jsm8109jsm <jsm8109jsm@gmail.com>
2022-09-11 18:04:50 +0900

    Add: Todo 화면에 표시

--- src/Components/Todo.tsx
+++ src/Components/Todo.tsx
@@ -5,85 +5,144 @@ import "../styles/Todo.css";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 
-function MyTodo({teamId, func} : {teamId: string, func: any}) {
+interface Todo {
+  id: number;
+  completed: boolean;
+  nickname: string;
+  createdAt: string;
+  endAt: string;
+  title: string;
+  todo: string;
+}
 
+function MyTodo({ teamId, func }: { teamId: string; func: any }) {
   const postTodo = async () => {
-    const result = await axios.post('/api/todo/upload', input);
-    if(result != null) {
+    const result = await axios.post("/api/todo/upload", input);
+    if (result != null) {
       func();
+    } else {
+      setText("실패했습니다.");
     }
-    else{
-      setText('실패했습니다.');
-    }
-  }
+  };
 
-  const changeData = (e: 
-    | React.ChangeEvent<HTMLInputElement>
-    | React.ChangeEvent<HTMLTextAreaElement>
-    ) => {
-      const {name, value} = e.target;
-      const nextInput = {
-        ...input,
-        [name]: value,
-      };
-      setInput(nextInput);
-  }
+  const changeData = (
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    const nextInput = {
+      ...input,
+      [name]: value,
+    };
+    setInput(nextInput);
+  };
 
   const [input, setInput] = useState({
-    title: '',
-    todo: '',
-    endAt: '',
+    title: "",
+    todo: "",
+    endAt: "",
     teamId: teamId,
   });
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
 
-  return(
+  return (
     <div className="mytodo-root">
       <h1 className="mytodo-title">할 일 등록</h1>
       <ul className="mytodo-ul">
         <li className="mytodo-li">
           <span className="mytodo-span">할 일</span>
-          <input type="text" placeholder="할 일명을 입력하세요." className="mytodo-input" name="title" onChange={(e)=>{ changeData(e) }} />
+          <input
+            type="text"
+            placeholder="할 일명을 입력하세요."
+            className="mytodo-input"
+            name="title"
+            onChange={(e) => {
+              changeData(e);
+            }}
+          />
         </li>
         <li className="mytodo-li">
           <span className="mytodo-span">설명</span>
-          <textarea placeholder="설명을 입력하세요." className="mytodo-textarea" name="todo" onChange={(e)=>{ changeData(e) }} />
+          <textarea
+            placeholder="설명을 입력하세요."
+            className="mytodo-textarea"
+            name="todo"
+            onChange={(e) => {
+              changeData(e);
+            }}
+          />
         </li>
         <li className="mytodo-li">
           <span className="mytodo-span">마감일</span>
-          <input type="date" className="mytodo-input" name="endAt" onChange={(e)=>{ changeData(e) }} />
+          <input
+            type="date"
+            className="mytodo-input"
+            name="endAt"
+            onChange={(e) => {
+              changeData(e);
+            }}
+          />
         </li>
         <div className="mytodo-button-div">
-          <button className="mytodo-button" onClick={postTodo}>등록</button>
-          <button className="mytodo-button" onClick={()=>{func()}}>취소</button>
+          <button className="mytodo-button" onClick={postTodo}>
+            등록
+          </button>
+          <button
+            className="mytodo-button"
+            onClick={() => {
+              func();
+            }}
+          >
+            취소
+          </button>
         </div>
       </ul>
       <span>{text}</span>
     </div>
-  )
+  );
 }
 
+const TodoList = ({ item }: { item: Todo }) => {
+  console.log(item);
+  const { title, completed, todo } = item;
+  const [complete, setComplete] = useState("완료됨");
+  // useEffect(() => {
+  //   completed ? setComplete("완료됨") : setComplete("진행중");
+  // }, []);
+  return (
+    <div>
+      <div className="Todo-content-list-line">
+        <span className="Todo-content-list-line-todo">{title}</span>
+        <span className="Todo-content-list-line-complete">{complete}</span>
+      </div>
+    </div>
+  );
+};
 
 function Todo() {
   const [myTodoModal, setMyTodoModal] = useState(false);
-  const [todo, setTodo] = useState({
-    text: "1페이지 수정 후 업로드",
-    complete: "진행 예정",
-  });
-  const { text, complete } = todo;
+  const [todo, setTodo] = useState<Todo[]>([]);
   const param = useParams();
 
   const addMyTodo = () => {
     setMyTodoModal((prev) => !prev);
-  }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await getTodo();
+        setTodo(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
 
-  }, [])
-
-  const getTodo = () =>{
+  const getTodo = () => {
     return axios.get(`/api/todo/${param.teamId}`);
-  }
+  };
   return (
     <div className="Todo-root">
       <TeamHeader />
@@ -94,25 +153,20 @@ function Todo() {
           <h1 className="Todo-title">TODO</h1>
         </div>
         <div className="Todo-content-div">
-        {myTodoModal ? <MyTodo teamId={param.teamId} func={addMyTodo}></MyTodo> : <></>}
+          {myTodoModal ? (
+            <MyTodo teamId={param.teamId} func={addMyTodo}></MyTodo>
+          ) : (
+            <></>
+          )}
           <div className="Todo-content">
             <div className="Todo-content-header">
               <h1 className="Todo-content-title">MY TODO</h1>
               <TiPlus size={24} onClick={addMyTodo} />
             </div>
             <div className="Todo-content-list">
-              <div className="Todo-content-list-line">
-                <span className="Todo-content-list-line-todo">{text}</span>
-                <span className="Todo-content-list-line-complete">
-                  {complete}
-                </span>
-              </div>
-              <div className="Todo-content-list-line">
-                <span className="Todo-content-list-line-todo">{text}</span>
-                <span className="Todo-content-list-line-complete">
-                  {complete}
-                </span>
-              </div>
+              {todo.map((item: Todo) => {
+                return <TodoList item={item} key={item.id} />;
+              })}
             </div>
           </div>
           <div className="Todo-content">
