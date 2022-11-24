ddba98a779336f84a5176fcb8bd998fe009c27cc
jsm8109jsm <jsm8109jsm@gmail.com>
2022-11-24 20:02:05 +0900

    FIX :: 댓글 디자인수정

--- src/Components/comment/comment.js
+++ src/Components/comment/comment.js
@@ -4,64 +4,66 @@ import { useState } from 'react';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
-function PostComment({postComment})
-{
-    return(
+function PostComment({ postComment }) {
+    return (
         <div>
 
         </div>
     )
 }
 
-export default function Comment({ postId, comment, deleteComment })
-{
+export default function Comment({ postId, comment, deleteComment }) {
     const [input, setInput] = useState("");
     const [change, setChange] = useState(false);
     const createMarkup = () => {
-        return {__html: comment.content};
+        return { __html: comment.content };
     }
 
     const postComment = () => {
-        setChange((prev)=>!prev);
+        setChange((prev) => !prev);
     }
 
     const created = comment.createdAt;
-    const hour = Number(comment.createdAt.substring(12, 13))+9;
+    const hour = Number(comment.createdAt.substring(12, 13)) + 9;
     const realHour = Number(hour) < 10 ? "0" + String(hour) : String(hour);
-    const date = created.substring(0, 10) + " " + realHour + "시 " + created.substring(14, 16) + "분 " + created.substring(17, 19)+"초";
+    const date = created.substring(0, 10) + " " + realHour + "시 " + created.substring(14, 16) + "분 " + created.substring(17, 19) + "초";
 
 
-    return(
+    return (
         <>
-            <div className="read-comment">
-                <p className="read-comment-nickname">{comment.nickname}</p>
-                <p className="read-comment-date">{date}</p>
-                <p className="read-comment-content" dangerouslySetInnerHTML={createMarkup()}></p>
-                {comment.permission && <button onClick={()=> deleteComment(comment.id) } className="read-comment-delete">
-                    삭제
-                    </button>}
-                <button className='read-comment-post' onClick={postComment} >댓글 달기</button>
-                {change && 
-                <div className="write-comment">
-                    <CKEditor
-                    editor={ClassicEditor}
-                    data={input}
-                    onChange={(event, editor) => {
-                        const data = editor.getData();
-                        setInput(data);
-                    }}
-                    />
-                </div>}
-            {comment.child && comment.child.map((value)=>{
-                if (value.deleted !== true) {
-                    return <Recomment postId={postId} comment={value} deleteComment={deleteComment} />
-                }
-                else {
-                return <div className="deleted-comment">
-                        <p>삭제된 댓글입니다.</p>
+            <div className="read-comment" onClick={postComment} >
+                <div className='read-comment-div'>
+                    <img src={`https://auth.bssm.kro.kr/resource/user/profile/${comment.usercode}.png`} alt='유저 프로필' className='comment-profileImg' />
+                    <div>
+                        <p className="read-comment-nickname">{comment.nickname}</p>
+                        <p className="read-comment-date">{date}</p>
                     </div>
-                }
-            })}
+                </div>
+                <p className="read-comment-content" dangerouslySetInnerHTML={createMarkup()}></p>
+                {comment.permission && <button onClick={() => deleteComment(comment.id)} className="read-comment-delete">
+                    삭제
+                </button>}
+                {change &&
+                    <div className="write-comment">
+                        <CKEditor
+                            editor={ClassicEditor}
+                            data={input}
+                            onChange={(event, editor) => {
+                                const data = editor.getData();
+                                setInput(data);
+                            }}
+                        />
+                    </div>}
+                {comment.child && comment.child.map((value) => {
+                    if (value.deleted !== true) {
+                        return <Recomment postId={postId} comment={value} deleteComment={deleteComment} />
+                    }
+                    else {
+                        return <div className="deleted-comment">
+                            <p>삭제된 댓글입니다.</p>
+                        </div>
+                    }
+                })}
             </div>
         </>
     )
