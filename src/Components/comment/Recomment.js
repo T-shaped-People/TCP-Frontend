bc268492883330593cf5b4f70249b9cbbe3c01e8
leehj050211 <leehj050211@gmail.com>
2022-11-30 17:11:25 +0900

    FIX: 댓글 프로필 사진 버그

--- src/Components/comment/Recomment.js
+++ src/Components/comment/Recomment.js
@@ -24,26 +24,31 @@ export default function Recomment({ postId, comment, deleteComment })
         }
     `
 
-    return(
-        <>
-            <ReReadComment>
-                <p className="re-read-comment-nickname">{comment.nickname}</p>
-                <p className="re-read-comment-date">{elapsedTime(comment.createdAt, MilliSecondTime.MONTH)}</p>
-                <p className="re-read-comment-content" dangerouslySetInnerHTML={createMarkup()}></p>
-                {comment.permission && <button onClick={()=> deleteComment(comment.id) } className="re-read-comment-delete">
-                    삭제
-                    </button>}
-                    {comment.child && comment.child.map((value)=>{
-                if (value.deleted !== true) {
-                    return <Recomment postId={postId} comment={value} deleteComment={deleteComment} />
-                }
-                else {
-                return <div className="deleted-comment">
-                        <p>삭제된 댓글입니다.</p>
-                    </div>
-                }
-            })}
-            </ReReadComment>
-        </>
-    )
+    return (
+        <ReReadComment>
+            <div className='read-comment-div'>
+                <img
+                    src={`https://auth.bssm.kro.kr/resource/user/profile/${comment.usercode}.png`}
+                    onError={e => e.currentTarget.src = '/images/profile_default.png'}
+                    alt='유저 프로필'
+                    className='comment-profileImg'
+                />
+                <div>
+                    <p className="read-comment-nickname">{comment.nickname}</p>
+                    <p className="read-comment-date">{elapsedTime(comment.createdAt, MilliSecondTime.MONTH)}</p>
+                </div>
+            </div>
+            <p className="read-comment-content" dangerouslySetInnerHTML={createMarkup()}></p>
+            {comment.permission && <button onClick={()=> deleteComment(comment.id) } className="re-read-comment-delete">
+                삭제
+            </button>}
+            {comment.child && comment.child.map(value => (
+                value.deleted
+                ? <div className="deleted-comment">
+                    <p>삭제된 댓글입니다.</p>
+                </div>
+                : <Recomment postId={postId} comment={value} deleteComment={deleteComment} />
+            ))}
+        </ReReadComment>
+    );
 }
--- src/Components/comment/comment.js
+++ src/Components/comment/comment.js
@@ -5,14 +5,6 @@ import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { elapsedTime, MilliSecondTime } from '../../utils/date';
 
-function PostComment({ postComment }) {
-    return (
-        <div>
-
-        </div>
-    )
-}
-
 export default function Comment({ postId, comment, deleteComment }) {
     const [input, setInput] = useState("");
     const [change, setChange] = useState(false);
@@ -29,41 +21,44 @@ export default function Comment({ postId, comment, deleteComment }) {
 
 
     return (
-        <>
-            <div className="read-comment" onClick={postComment} >
-                <div className='read-comment-div'>
-                    <img src={`https://auth.bssm.kro.kr/resource/user/profile/${comment.usercode}.png`} alt='유저 프로필' className='comment-profileImg' />
-                    <div>
-                        <p className="read-comment-nickname">{comment.nickname}</p>
-                        <p className="read-comment-date">{elapsedTime(comment.createdAt, MilliSecondTime.MONTH)}</p>
-                    </div>
+        <div className="read-comment" onClick={postComment} >
+            <div className='read-comment-div'>
+                <img
+                    src={`https://auth.bssm.kro.kr/resource/user/profile/${comment.usercode}.png`}
+                    onError={e => e.currentTarget.src = '/images/profile_default.png'}
+                    alt='유저 프로필'
+                    className='comment-profileImg'
+                />
+                <div>
+                    <p className="read-comment-nickname">{comment.nickname}</p>
+                    <p className="read-comment-date">{elapsedTime(comment.createdAt, MilliSecondTime.MONTH)}</p>
                 </div>
-                <p className="read-comment-content" dangerouslySetInnerHTML={createMarkup()}></p>
-                {comment.permission && <button onClick={() => deleteComment(comment.id)} className="read-comment-delete">
-                    삭제
-                </button>}
-                {change &&
-                    <div className="write-comment">
-                        <CKEditor
-                            editor={ClassicEditor}
-                            data={input}
-                            onChange={(event, editor) => {
-                                const data = editor.getData();
-                                setInput(data);
-                            }}
-                        />
-                    </div>}
-                {comment.child && comment.child.map((value) => {
-                    if (value.deleted !== true) {
-                        return <Recomment postId={postId} comment={value} deleteComment={deleteComment} />
-                    }
-                    else {
-                        return <div className="deleted-comment">
-                            <p>삭제된 댓글입니다.</p>
-                        </div>
-                    }
-                })}
             </div>
-        </>
+            <p className="read-comment-content" dangerouslySetInnerHTML={createMarkup()}></p>
+            {comment.permission && <button onClick={() => deleteComment(comment.id)} className="read-comment-delete">
+                삭제
+            </button>}
+            {change &&
+                <div className="write-comment">
+                    <CKEditor
+                        editor={ClassicEditor}
+                        data={input}
+                        onChange={(event, editor) => {
+                            const data = editor.getData();
+                            setInput(data);
+                        }}
+                    />
+                </div>}
+            {comment.child && comment.child.map((value) => {
+                if (value.deleted !== true) {
+                    return <Recomment postId={postId} comment={value} deleteComment={deleteComment} />
+                }
+                else {
+                    return <div className="deleted-comment">
+                        <p>삭제된 댓글입니다.</p>
+                    </div>
+                }
+            })}
+        </div>
     )
 }
